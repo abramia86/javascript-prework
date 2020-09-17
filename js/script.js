7aa69510c7a849355a44708a294176a553f566fd
Bartosz Abramczyk <abramczyk.bartosz86@gmail.com>
2020-09-17 20:59:22 +0200

    wybieranie ruchów

--- js/script.js
+++ js/script.js
@@ -8,11 +8,35 @@ function clearMessages(){
 	document.getElementById('messages').innerHTML = '';
 }
 
-let computerMove = "kamień";
+let randomNumber = Math.floor(Math.random() * 3 + 1);
 
-printMessage("Zagrałem" + computerMove + "! Jeśli twój ruch to papier to wygrywasz");
+console.log("Wylosowana liczba to:" + randomNumber);
 
-let playerMove = "papier";
+let computerMove = 'nieznany ruch';
 
-printMessage("Zagrałem" + playerMove + "! Jeśli twój ruch to papier to wygrywasz");
+if (randomNumber == 1){
+	computerMove = "kamień";
+} else if(randomNumber == 2){
+	computerMove = "papier";
+} else if(randomNumber == 3){
+	computerMove = "nożyce";
+}
 
+printMessage("mój ruch to:" + computerMove);
+
+let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+
+console.log("Gracz wpisał:" + playerInput);
+
+let playerMove = 'nieznany ruch';
+
+if (playerInput == '1') {
+	playerMove = 'kamień';
+} else if(playerInput =="2") {
+	playerMove = "papier";
+} else if(playerInput =="3") {
+	playerMove = "nożyce";
+}
+
+
+printMessage('Twoj ruch to:' + playerMove);
